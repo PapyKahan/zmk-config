def2486b62f7275121c2191ee42701277b375663
PapyKahan <fajardo.christophe@gmail.com>
2025-02-13 09:42:30 +0100

    fix(cyber60): update base code

--- config/boards/arm/cyber60_rev_d/cyber60_buzzer.c
+++ config/boards/arm/cyber60_rev_d/cyber60_buzzer.c
@@ -16,10 +16,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #include <zmk/ble.h>
 #include <zmk/event_manager.h>
-#include <zmk/endpoints.h>
-#include <zmk/endpoints_types.h>
 #include <zmk/events/ble_active_profile_changed.h>
-#include <zmk/events/endpoint_changed.h>
 
 #define BUZZER_NODE DT_ALIAS(buzzer)
 
@@ -29,22 +26,25 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 #endif
 
-#define BEEP_DURATION K_MSEC(60)
+#define BEEP_DURATION  K_MSEC(60)
 
 static const struct pwm_dt_spec pwm = PWM_DT_SPEC_GET(BUZZER_NODE);
 
-static void _play(uint32_t period) {
-    if (!device_is_ready(pwm.dev)) {
-        printk("Error: PWM device %s is not ready\n", pwm.dev->name);
-        return;
-    }
+void _play(uint32_t period)
+{
+	if (!device_is_ready(pwm.dev)) {
+		printk("Error: PWM device %s is not ready\n",
+		       pwm.dev->name);
+		return;
+	}
     pwm_set_dt(&pwm, period, period / 2U); // attempt at new implementation
     k_sleep(BEEP_DURATION);
     pwm_set_dt(&pwm, 0, 0); // attempt at new implementation
     k_sleep(K_MSEC(50));
 }
 
-static void play_sound_ble_0() {
+void play_sound_1()
+{
     _play(1000000);
     _play(500000);
     _play(250000);
@@ -52,77 +52,61 @@ static void play_sound_ble_0() {
     _play(50000);
 }
 
-static void play_sound_ble_1() {
+void play_sound_2()
+{
     _play(1500000);
     _play(3900000);
     _play(1500000);
     _play(1500000);
 }
 
-static void play_sound_ble_2() {
+void play_sound_3()
+{
     _play(1500000);
     _play(3900000);
 }
 
-static void play_sound_ble_3() {
+void play_sound_4()
+{
     _play(2000000);
     _play(3900000);
 }
 
-static void play_sound_ble_4() {
+void play_sound_5()
+{
     _play(2500000);
-    _play(3900000);
+    _play(3900000);               
 }
 
-static void play_sound_usb() {
-    _play(3000000);
-    _play(1500000);
-    _play(750000);
-}
-
-static void play_sound(uint8_t index) {
-    switch (index) {
-    case 0:
-        play_sound_ble_0();
-        break;
-    case 1:
-        play_sound_ble_1();
-        break;
-    case 2:
-        play_sound_ble_2();
-        break;
-    case 3:
-        play_sound_ble_3();
-        break;
-    case 4:
-        play_sound_ble_4();
-        break;
-    case 5:
-        play_sound_usb();
-        break;
-    default:
-        break;
+int buzzer_listener(const zmk_event_t *eh)
+{
+    const struct zmk_ble_active_profile_changed *profile_ev = NULL;
+    if ((profile_ev = as_zmk_ble_active_profile_changed(eh)) == NULL) {
+        return ZMK_EV_EVENT_BUBBLE;
     }
-}
-
-int buzzer_listener(const zmk_event_t *eh) {
-    static int index = -1;
-    int new_index = -1;
-    switch (zmk_endpoints_selected().transport) {
-    case ZMK_TRANSPORT_USB:
-        new_index = 5;
-        break;
-    case ZMK_TRANSPORT_BLE:
-        new_index = zmk_ble_active_profile_index();
-        break;
-    }
-    if (new_index != index) {
-        play_sound(new_index);
-        index = new_index;
+    switch(profile_ev->index) {
+        case 0:
+            play_sound_1();
+            break;
+        case 1:
+            play_sound_2();
+            break;
+        case 2:
+            play_sound_3();
+            break;
+        case 3:
+            play_sound_4();
+            break;
+        case 4:
+            play_sound_5();
+            break;
+        default:
+            break;
     }
     return ZMK_EV_EVENT_BUBBLE;
 }
 
 ZMK_LISTENER(buzzer_output_status, buzzer_listener)
-ZMK_SUBSCRIPTION(buzzer_output_status, zmk_ble_active_profile_changed);
-ZMK_SUBSCRIPTION(buzzer_output_status, zmk_endpoint_changed);
+#if defined(CONFIG_ZMK_BLE)
+    ZMK_SUBSCRIPTION(buzzer_output_status, zmk_ble_active_profile_changed);
+#endif
